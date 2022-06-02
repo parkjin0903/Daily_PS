c6e48813cdea5551727deec03901949610c896ad
parkjin0903 <103939460+parkjin0903@users.noreply.github.com>
2022-06-02 17:45:53 +0900

    01.ps step7-3

--- 01.ps step7-2.py
+++ 01.ps step7-2.py
@@ -1,10 +1,14 @@
-n = int(input())
-cnt = 1
-cnt_six = 6
-count = 1
-while n > cnt:
-    count += 1
-    cnt += cnt_six
-    cnt_six += 6
+X = int(input()) #3
+
+X + 1
+
+for i in range(1, X+1):
     
-print(count)
+    
+    
+1/1 
+1/2 2/1
+3/1 2/2 1/3
+1/4 2/3 3/2 4/1
+
+ 
