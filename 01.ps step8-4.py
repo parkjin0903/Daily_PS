6310634a565470ffaa7d9c997a5f08bfeff264b3
parkjin0903 <103939460+parkjin0903@users.noreply.github.com>
2022-06-09 12:06:40 +0900

    01.ps step8-4

---
+++ 01.ps step8-4.py
@@ -0,0 +1,12 @@
+M, N = map(int, input().split())
+for number in range(M, N+1):  
+  if number == 1:
+    pass
+  
+  for divide_num in range(2, int(number ** 0.5)+1):
+    if number % divide_num == 0:
+      break
+      
+  else:
+    print(number)
+  
