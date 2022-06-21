f654d14de0f8faeade48564406efa0e811271052
parkjin0903 <103939460+parkjin0903@users.noreply.github.com>
2022-06-21 17:36:32 +0900

    01.ps step11-5

--- 01.ps step11-5.py
+++ 01.ps step11-5.py
@@ -1,4 +1,14 @@
-a = list(map(int, str(input())))
-a.sort(reverse=True)
-for i in a:
-  print(i , end='')
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+array_list = []
+for i in range(n):
+  [a, b] = map(int, input().split())
+  array_list.append([a, b])
+
+array_list.sort()
+
+for list_index in range(n):
+  print(array_list[i][0], array_list[i][1])
