1d52d25e0a1d7ef0046f5212b9156bf194ccf696
parkjin0903 <103939460+parkjin0903@users.noreply.github.com>
2022-06-23 21:38:32 +0900

    01.ps step11-7

--- 01.ps step11-7.py
+++ 01.ps step11-7.py
@@ -6,7 +6,7 @@ num_list = []
 for i in range(n):
     num_list.append(sys.stdin.readline().strip())
 set_list = set(num_list)
-num_list = num_list(set_list)
+num_list = list(set_list)
 num_list.sort()
 num_list.sort(key = len)
 
