6d3aae3f9e99e4675cd66ab29dd1bad20c3a61ad
parkjin0903 <103939460+parkjin0903@users.noreply.github.com>
2022-06-29 23:31:39 +0900

    01.ps step13-1

--- 01.ps step12-4.py
+++ 01.ps step12-4.py
@@ -1,20 +1,7 @@
-import sys
-
-n = sys.stdin.readline().strip()
-card = list(map(int, sys.stdin.readline().split()))
-m = sys.stdin.readline().strip()
-test = list(map(int, sys.stdin.readline().split()))
-
-dict = {}
-
-for i in card:  # {-10:2, 2:1, 3:2, 6:1, 7:1, 10:3}
-    if i not in dict:
-        dict[i] = 1
-    else:
-        dict[i] += 1
-
-for i in test:
-    if i in dict:
-        print(dict[i], end=" ")
-    else:
-        print(0, end=" ")
+x, y, w, h = amp(int, input().split())
+a = []
+a.append(x-0)
+a.append(y-0)
+a.append(w-x)
+a.append(h-y)
+print(min(a))
