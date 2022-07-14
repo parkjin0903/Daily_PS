1ca1fed20a2435178964d6b5bca9cd06e94d0720
parkjin0903 <103939460+parkjin0903@users.noreply.github.com>
2022-07-14 09:22:12 +0900

    01.ps step14-6

---
+++ 01.ps step14-6.py
@@ -0,0 +1,14 @@
+# 14-6
+
+M, N = map(int, input().split())
+
+def countnum(num1, num2):
+  count = 0
+  while num1 != 0 :
+    num1 = num1 // num2
+    count += num1
+  return count 
+
+print(min(countnum(M, 2) - countnum(M-N, 2) - countnum(N, 2), countnum(M, 5)- countnum(M-N, 5) - countnum(N, 5))) 
+
+
