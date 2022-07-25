0dd7cc5df59c8f63efb9520a267cbc36ff3cfa04
parkjin0903 <103939460+parkjin0903@users.noreply.github.com>
2022-07-25 21:41:22 +0900

    01.ps step16-5

---
+++ 01.ps step16-5.py
@@ -0,0 +1,12 @@
+# 16-5
+
+N = int(input())
+arr = list(map(int, input().split()))
+dp = [0 for i in range(N)]
+
+for i in range(N):
+    for j in range(i):
+        if arr[i] > arr[j] and dp[i] < dp[j]:
+            dp[i] = dp[j]
+    dp[i] += 1
+print(max(dp))
