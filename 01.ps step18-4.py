b5cde0d6115bb53eccc99a2ffc05c998bab9289c
parkjin0903 <103939460+parkjin0903@users.noreply.github.com>
2022-07-30 17:15:00 +0900

    01.ps step18-4

---
+++ 01.ps step18-4.py
@@ -0,0 +1,12 @@
+# 18-4
+def main():
+
+    mathexp_list = input().split('-') # ['55+30', '50+40']
+    result = sum(map(int, (mathexp_list[0].split('+'))))
+    for index_num in range(1, len(mathexp_list)):
+        result -= sum(map(int, (mathexp_list[index_num].split('+'))))
+    print(result)
+
+
+if __name__ == '__main__':
+    main() 
