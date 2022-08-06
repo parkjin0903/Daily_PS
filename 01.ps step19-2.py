d0921c42246beb3f33335c350df683a3bb822abd
Park Jin <pj09230903@gmail.com>
2022-08-06 15:49:35 +0900

    01.ps step19-2

---
+++ 01.ps step19-2.py
@@ -0,0 +1,16 @@
+# 19-2 
+import sys
+input = sys.stdin.readline
+
+money_list = []
+
+num = int(input())
+
+for _ in range(num):
+    input_num = int(input())
+    if input_num != 0:
+        money_list.append(input_num)
+    elif input_num == 0:
+        money_list.pop()
+
+print(sum(money_list))
