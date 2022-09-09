0474ed1ebe539b18b276aa846f449096c509157b
Park Jin <qkrwls0903@naver.com>
2022-09-09 14:54:22 +0900

    #1260

--- #1260.py
+++ #1260.py
@@ -7,7 +7,7 @@ input = sys.stdin.readline
 
 N, M, V = map(int, input().split())
 
-graph = [[] for _ in range(M+1)]
+graph = [[] for _ in range(N+1)]
 
 for _ in range(M):
     a, b = map(int, input().split())
