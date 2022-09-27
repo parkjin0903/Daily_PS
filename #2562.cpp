051e3ce6f8d04300401f731cfe9dbec8f2c1f04e
Park Jin <qkrwls0903@naver.com>
2022-09-27 10:30:25 +0900

    programmers_lv1_12910

--- #2562.cpp
+++ #2562.cpp
@@ -4,28 +4,7 @@ using namespace std;
 
 int main(){
 	
-	int n;
-	cin>>n;
-	
-	int num[n+1];
-	int min = 1000000;
-	int max = 0;     // 틀린 이유
-	
-	for(int i=0; i<n; i++){
-		cin >> num[i];
-		if(min < num[i]){  // 복잡한 조건문 사용
-			min = min;
-		} else {
-			min = num[i];
-		}
-		if(max > num[i]){
-			max = max;
-		} else {
-			max = num[i];
-		}
-	}
-	
-	cout << min << " " << max << "\n";
-	
+	int nam[42] = {0};
+	cout << nam;
 	return 0;
 }
