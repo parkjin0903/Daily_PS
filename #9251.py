0801fc4a8f2a3354c9f94a7a6b2cdcba0c260406
Park Jin <qkrwls0903@naver.com>
2022-10-19 23:51:14 +0900

    #1874

--- #9251.py
+++ #9251.py
@@ -1,17 +1,32 @@
 import sys
 input = sys.stdin.readline
 
-word1 = input()
-word2 = input()
+def main():
+    N = int(input())
+    temp = []
+    ans = 0
+    for i in range(N):
+        card = int(input())
+        temp.append(card)
 
-temp = [0] * len(word2)
+    temp.sort()
 
-for i in range(len(word1)):
-    cnt = 0
-    for j in range(len(word2)):
-        if cnt < temp[j]:
-            cnt = temp[j]
-        elif word1[i] == word2[j]:
-            temp[j] = cnt + 1
+    if N == 1:
+        return card
+    
+    elif N == 1:
+        return sum(temp)
+    
+    else:
+        cnt = N-2
+        ans += (temp[0] + temp[1]) * (N-1)
+        while cnt > 0:
+            for i in range(2, len(temp)-2):
+                ans += temp[i] * cnt
+                cnt -= 1
+        return ans
 
-print(max(temp)-1)
+if __name__ == '__main__':
+    main()
+    
+            
