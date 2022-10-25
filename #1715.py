a8168a3ced5eb3837dd220466b1172d65d7d7cf9
Park Jin <qkrwls0903@naver.com>
2022-10-25 14:49:52 +0900

    #10807

--- #1715.py
+++ #1715.py
@@ -1,27 +1,17 @@
-# 1715
-
 import sys
 import heapq
 input = sys.stdin.readline
 
-def main():
-    N = int(input())
-    temp = []
-    for i in range(N):
-        card = int(input())
-        heapq.heappush(temp, card)
-
-    if N == 1:
-        print(0)
-    else:
-        answer = 0
-        while len(temp) > 1:
-            temp1 = heapq.heappop(temp)
-            temp2 = heapq.heappop(temp)
-            answer += temp1 + temp2
-            heapq.heappush(temp, temp1 + temp2)
+N = int(input())
+for _ in range(N):
+    word = int(input())
+    list_temp = list(map(int, input().split()))
+    heapq.heapify(list_temp)
+    answer = 0
+    while len(list_temp) > 1:
+        temp1 = heapq.heappop(list_temp)
+        temp2 = heapq.heappop(list_temp)
+        answer += temp1 + temp2
+        heapq.heappush(list_temp, temp1 + temp2)
         print(answer)
-if __name__ == '__main__':
-    main()
-    
-            
+    print(answer)
