03379b79aa035e1dc9430741aa325993380d494d
Park Jin <qkrwls0903@naver.com>
2023-05-27 20:57:41 +0900

    #25192

--- #24723.py
+++ #24723.py
@@ -1,3 +1,24 @@
-N = int(input())
-print(2**N)
-    
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+count = 0
+list1 = []
+for _ in range(n):
+    temp = input().rstrip()
+    if temp == "ENTER":
+        set(list1)
+        count += len(list1)
+        list1 = []
+        
+    else:
+        list1.append(temp)
+        set(list1)
+        list(list1)
+    print(list1)
+
+count += len(list1)
+
+print(count)
