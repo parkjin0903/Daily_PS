de2636fc993d6b794852f10365f80367f66161cc
Park Jin <qkrwls0903@naver.com>
2023-05-29 22:58:05 +0900

    #1461

--- #2748.py
+++ #2748.py
@@ -1,10 +1,7 @@
-n = int(input())
+m, n = map(int, input().split())
 
-fibo =[] #피보나치 수를 저장하는 리스트
-fibo.append(0) #0과 1을 미리 저장
-fibo.append(1)
+books = list(map(int, input().split()))
 
-for i in range(2,n+1): #리스트에 피보나치 수를 계산하여 저장
-    fibo.append(fibo[i-1]+fibo[i-2])
+books.sort()
 
-print(fibo[n])
+print(books)
