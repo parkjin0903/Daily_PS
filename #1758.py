2c0d72752f8cf2686812845276b7745fc766134f
Park Jin <qkrwls0903@naver.com>
2023-08-25 23:47:22 +0900

    #1758

---
+++ #1758.py
@@ -0,0 +1,18 @@
+import sys
+ 
+n = int(sys.stdin.readline())
+ 
+li = []
+for i in range(n):
+    li.append(int(sys.stdin.readline()))
+ 
+li.sort(reverse=True)
+ 
+total = 0
+for i in range(n):
+    ans = li[i] - i
+    if ans < 0:
+        break
+    total += li[i] - i
+ 
+print(total)
